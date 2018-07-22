8ed71533479e9ec2f1868228cb9d39e97f53d520
ramasamyt <contact.ramasamy@gmail.com>
2018-07-22 11:16:11 -0400

    Uploaded ideashare website files

---
+++ JS/javascript.js
@@ -0,0 +1,62 @@
+//-- For Idea #1 --//
+//For Up 1 Button//
+var count11 = 0
+var button11 = document.getElementById('buttoncount11');
+var display11 = document.getElementById('up1');
+
+button11.onclick=function(){
+    count11++;
+    display11.innerHTML=count11;
+}
+
+//For Down 1 Button//
+var count12 = 0
+var button12 = document.getElementById('buttoncount12');
+var display12 = document.getElementById('dn1');
+
+button12.onclick=function(){
+    count12--;
+    display12.innerHTML=count12;
+}
+
+//-- For Idea #2--//
+//For Up 2 Button//
+var count21 = 0
+var button21 = document.getElementById('buttoncount21');
+var display21 = document.getElementById('up2');
+
+button21.onclick=function(){
+    count21++;
+    display21.innerHTML=count21;
+}
+
+//For Down 2 Button//
+var count22 = 0
+var button22 = document.getElementById('buttoncount22');
+var display22 = document.getElementById('dn2');
+
+button22.onclick=function(){
+    count22--;
+    display22.innerHTML=count22;
+}
+
+//-- For Idea #3 -- //
+//For Up 3 Button//
+var count31 = 0
+var button31 = document.getElementById('buttoncount31');
+var display31 = document.getElementById('up3');
+
+button31.onclick=function(){
+    count31++;
+    display31.innerHTML=count31;
+}
+
+//For Down 3 Button//
+var count32 = 0
+var button32 = document.getElementById('buttoncount32');
+var display32 = document.getElementById('dn3');
+
+button32.onclick=function(){
+    count32--;
+    display32.innerHTML=count32;
+}
